a95378b7f3760a08a6d94e346a2197221462408e
Fabio.Storer <fabiostorer19@gmail.com>
2024-06-18 16:19:08 -0300

    feat: codigo removido para correcao

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 let numeros = [3, 3, 4, 7, 8, 9, 1, 5, 2, 10, 2, 6];
 let numerosOrdem = [];
-let j = 1;
 
-for (let i = 0; i < numeros.length; i++) {
-    if (numeros[i] == numeros[j]) {
-        numeros.splice(numeros[i]);
-        console.log(numeros);
-    }
-  //  for (let c = 1; c < numeros.length; c++) {
-   //     if (numeros[i] < numeros[c]) {
-       //     numerosOrdem.push(numeros);
-   //     }
-   // }
-}
-console.log(numerosOrdem);
