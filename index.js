fc43ac1e82f5a7cfbebbd25fd0ab2fafcfc5637a
Fabio.Storer <fabiostorer19@gmail.com>
2024-06-18 16:40:52 -0300

    feat: adicionado codigo funcional

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 let numeros = [3, 3, 4, 7, 8, 9, 1, 5, 2, 10, 2, 6];
-let numerosOrdem = [];
 
+for (let i = 0; i < numeros.length; i++) {
+    for (let j = i + 1; j < numeros.length; j++){
+        if (numeros[i] > numeros[j]) {
+            let aux = numeros[i];
+            numeros[i] = numeros[j];
+            numeros[j] = aux;
+        }
+    }
+}
+
+console.log(numeros);
